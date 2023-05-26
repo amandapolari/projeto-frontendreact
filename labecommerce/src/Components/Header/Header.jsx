e2796934c9579d8f743482821ac5667ffcc9c6a4
Amanda <amandaoliveirapolari@gmail.com>
2023-05-26 18:42:55 -0300

    Ajuste do caminhos das imagens provisórias + criação da pasta assets

--- labecommerce/src/Components/Header/Header.jsx
+++ labecommerce/src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Filters from '../Filters/Filters';
 import { ContainerHeader, IconCart, ImgHeader } from './HeaderSyle';
-import logo from '../../img/logo.png';
-import imgCart from '../../img/cart.png';
-// import images from '../../images.json';
+
+import logo from '../../assets/img/logo.png';
+import imgCart from '../../assets/img/cart.png';
 
 const Header = () => {
     return (
