abae8b6e3a9a508ce3b0dcca1ce498b23f60195d
Amanda <amandaoliveirapolari@gmail.com>
2023-05-30 16:33:54 -0300

    Implementação dos inputs controlados no componente Filters.js

--- labecommerce/src/Components/Filters/Filters.jsx
+++ labecommerce/src/Components/Filters/Filters.jsx
@@ -14,15 +14,56 @@ function Filters({
     //     setMaxFilter,
     //     searchFilter,
     //     setSearchFilter,);
+
+    // TRATATIVAS PARA NÚMEROS NEGATIVOS
+    const treatmentNegativeNumber = (event, functionSetFilter) => {
+        let enteredValue = Number(event.target.value);
+        if (enteredValue < 0) {
+            functionSetFilter('');
+        } else {
+            functionSetFilter(enteredValue);
+        }
+    };
+    // - - - - - - - - - - - - - -
+
+    // FUNÇÕES QUE MUDAM OS ESTADOS
+    const handleSearchFilterChanges = (event) => {
+        setSearchFilter(event.target.value);
+    };
+
+    const handleMaxFilteChanges = (event) => {
+        treatmentNegativeNumber(event, setMaxFilter);
+    };
+
+    const handleMinFilterChanges = (event) => {
+        treatmentNegativeNumber(event, setMinFilter);
+    };
+    // - - - - - - - - - - - - - -
+
     return (
         <ContainerFilter>
             <form action="">
-                <label htmlFor="">Buscar por nome</label>
-                <input type="text" />
-                <label htmlFor="">Valor Mínimo</label>
-                <input type="text" />
-                <label htmlFor="">Valor Máximo</label>
-                <input type="text" />
+                <label htmlFor="inputSearchFilter">Buscar por nome</label>
+                <input
+                    type="text"
+                    id="inputSearch"
+                    onChange={handleSearchFilterChanges}
+                    value={searchFilter}
+                />
+                <label htmlFor="inputMinFilter">Valor Mínimo</label>
+                <input
+                    type="number"
+                    id="inputMinFilter"
+                    onChange={handleMinFilterChanges}
+                    value={minFilter}
+                />
+                <label htmlFor="inputMaxFilter">Valor Máximo</label>
+                <input
+                    type="number"
+                    id="inputMaxFilter"
+                    onChange={handleMaxFilteChanges}
+                    value={maxFilter}
+                />
             </form>
         </ContainerFilter>
     );
