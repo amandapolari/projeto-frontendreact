c8f047941371d4d8efdcd210facc544a98752eaa
Amanda <amandaoliveirapolari@gmail.com>
2023-06-21 18:01:29 -0300

    Adição de fotos de blusas do espaço

--- labecommerce/src/Components/ProductCard/ProductCardStyle.js
+++ labecommerce/src/Components/ProductCard/ProductCardStyle.js
@@ -1,26 +1,28 @@
-import * as styled from 'styled-components';
+import styled from 'styled-components';
 
-export const CardContainer = styled.styled.div`
+export const CardContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
-    `;
-export const Card = styled.styled.div`
+`;
+export const Card = styled.div`
     border: solid 1.2px black;
     align-items: center;
-    width:20vw;
+    width: 15vw;
 `;
 
-export const Img = styled.styled.img`
-    width:20vw;
+export const Img = styled.img`
+    width: 15vw;
+    height: 35vh;
 `;
 
-export const Button = styled.styled.button`
+export const Button = styled.button`
+    width: 100%;
     /* display: grid; */
     /* justify-self: center; */
 `;
 
-export const ContainerSuperior = styled.styled.div`
+export const ContainerSuperior = styled.div`
     padding: 0 3vw 0 3vw;
     width: 100%;
     display: flex;
@@ -28,13 +30,13 @@ export const ContainerSuperior = styled.styled.div`
     align-items: center;
 `;
 
-export const P = styled.styled.p`
+export const P = styled.p`
     /* margin:2vw; */
     /* width:50%; */
     /* display: block; */
 `;
 
-export const Select = styled.styled.select`
+export const Select = styled.select`
     height: 3vh;
     /* margin:2vw; */
     /* width:50%; */
--- labecommerce/src/assets/productsList.js
+++ labecommerce/src/assets/productsList.js
@@ -1,21 +1,74 @@
+import blusa1 from './img/products/blusa-1.png';
+import blusa2 from './img/products/blusa-2.png';
+import blusa3 from './img/products/blusa-3.png';
+import blusa4 from './img/products/blusa-4.png';
+import blusa5 from './img/products/blusa-5.png';
+import blusa6 from './img/products/blusa-6.png';
+import blusa7 from './img/products/blusa-7.png';
+import blusa8 from './img/products/blusa-8.png';
+import blusa9 from './img/products/blusa-9.png';
+import blusa10 from './img/products/blusa-10.png';
+
 const productsList = [
     {
         id: 1,
-        name: 'produto 1',
-        value: 10000.0,
-        imageUrl: 'https://picsum.photos/300/335?a=1',
+        name: 'Camiseta Astronauta',
+        value: 29.99,
+        imageUrl: blusa1,
     },
     {
         id: 2,
-        name: 'Exemplo 2',
-        value: 5000.0,
-        imageUrl: 'https://picsum.photos/300/335?a=2',
+        name: 'Camiseta Alienígena',
+        value: 24.99,
+        imageUrl: blusa2,
     },
     {
         id: 3,
-        name: 'Item 3',
-        value: 500.0,
-        imageUrl: 'https://picsum.photos/300/335?a=3',
+        name: 'Camiseta Planetas',
+        value: 19.99,
+        imageUrl: blusa3,
+    },
+    {
+        id: 4,
+        name: 'Camiseta UFO',
+        value: 22.99,
+        imageUrl: blusa4,
+    },
+    {
+        id: 5,
+        name: 'Camiseta Astronomia',
+        value: 26.99,
+        imageUrl: blusa5,
+    },
+    {
+        id: 6,
+        name: 'Camiseta Nebulosa',
+        value: 21.99,
+        imageUrl: blusa6,
+    },
+    {
+        id: 7,
+        name: 'Camiseta Planetas em Cartoons',
+        value: 18.99,
+        imageUrl: blusa7,
+    },
+    {
+        id: 8,
+        name: 'Camiseta Exploração Espacial',
+        value: 24.99,
+        imageUrl: blusa8,
+    },
+    {
+        id: 9,
+        name: 'Camiseta Astronauta Abduzido',
+        value: 29.99,
+        imageUrl: blusa9,
+    },
+    {
+        id: 10,
+        name: 'Camiseta Estrelas Cadentes',
+        value: 19.99,
+        imageUrl: blusa10,
     },
 ];
 
