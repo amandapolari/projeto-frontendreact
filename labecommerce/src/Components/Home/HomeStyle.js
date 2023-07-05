d488e4d51dfbef205f9c88f6f16daa2fdd347582
Amanda <amandaoliveirapolari@gmail.com>
2023-07-05 17:03:15 -0300

    Menos cards sendo mostrados conforme a tela de tablet e celular

--- labecommerce/src/Components/Home/HomeStyle.js
+++ labecommerce/src/Components/Home/HomeStyle.js
@@ -4,28 +4,8 @@ export const ContainerHome = styled.div`
     padding: 1vh;
     display: flex;
     flex-direction: column;
-    @media (max-width: 768px) {
-        /* Estilo para tablets */
-        /* padding: 1vh; */
-    }
-
-    @media (max-width: 480px) {
-        /* Estilo para celulares */
-        /* padding: 3vh; */
-    }
 `;
 
 export const ContainerProduct = styled.div`
     min-height: 81vh;
-
-    @media (max-width: 768px) {
-        /* border:solid 2px black; */
-        /* Estilo para tablets */
-        /* min-height: 80vh; */
-    }
-
-    @media (max-width: 480px) {
-        /* Estilo para celulares */
-        /* min-height: 81vh; */
-    }
 `;
